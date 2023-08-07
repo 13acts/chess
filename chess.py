7fa6624bc4fc86b463733d0e46a84dd25440bd45
13acts <tv2k@gmail.com>
2023-08-07 23:50:03 +0700

    Rework i, j system

--- chess.py
+++ chess.py
@@ -1,13 +1,36 @@
 from pieces import *
 
 
+EMT = None # represents empty
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[RB1, NB1, BB1, QB , KB , BB2, NB2, RB2],
+            [PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8],
+            [EMT, EMT, EMT, EMT, EMT, EMT, EMT, EMT],
+            [EMT, EMT, EMT, EMT, EMT, EMT, EMT, EMT],
+            [EMT, EMT, EMT, EMT, EMT, EMT, EMT, EMT],
+            [EMT, EMT, EMT, EMT, EMT, EMT, EMT, EMT],
+            [PW1, PW2, PW3, PW4, PW5, PW6, PW7, PW8],
+            [RW1, NW1, BW1, QW , KW , BW2, NW2, RW2]
+            ]
+# Location of pieces
+white_pieces = set()
+for i in range(0, 8):
+    for j in range(6, 8):
+
+black_pieces = set()
+
 # white pieces
-PW = Pawn() # represents white pawn 
-KW = " ♔ " # represents white king
-QW = " ♕ " # represents white queen 
-RW = " ♖ " # represents white rook 
-BW = " ♗ " # represents white bishop 
-NW = " ♘ " # represents white knight
+KW = King() # represents white king
+QW = Queen() # represents white queen 
+RW1 = Rook() # represents white rook 
+RW2 = Rook() # represents white rook 
+BW1 = Bishop() # represents white bishop 
+BW2 = Bishop() # represents white bishop 
+NW1 = Knight() # represents white knight
+NW2 = Knight() # represents white knight
 
 # black pieces
 PB = " ♟︎ " # represents black pawn
@@ -18,4 +41,3 @@ BB = " ♝ " # represents black bishop
 NB = " ♞ " # represents black knight
 BLACK = "BLACK" 
 WHITE = "WHITE"
-E = None # represents empty 
--- pieces.py
+++ pieces.py
@@ -12,50 +12,50 @@ class Pawn():
     def available_moves(self, board):
         # REMEMBER TO ONLY CALL AFTER PROMOTE CHECK
         result = set()
-        x, y = self.position
+        i, j = self.position
         if self.side == "W":
             # 1 move ahead
-            if board[x][y+1] is None:
-                result.add((x, y+1))
+            if board[i+1][j] is None:
+                result.add((i+1, j))
                 # 2 moves ahead
-                if y + 2 < 8:
-                    if not self.moved and board[x][y+2] is None:
-                        result.add((x, y+2))
+                if i + 2 < 8:
+                    if not self.moved and board[i+2][j] is None:
+                        result.add((i+2, j))
             # Can eat opponent piece
-            if x + 1 < 8:
-                if board[x+1][y+1] in self.opponents:
-                    result.add((x+1, y+1))
-            if x - 1 > -1:
-                if board[x-1][y+1] in self.opponents:
-                    result.add((x-1, y+1))
+            if j + 1 < 8:
+                if board[i+1][j+1] in self.opponents:
+                    result.add((i+1, j+1))
+            if j - 1 > -1:
+                if board[i+1][j-1] in self.opponents:
+                    result.add((i+1, j-1))
             # en passant
             ...
             return result
         
         elif self.side == "B":
             # 1 move ahead
-            if board[x][y-1] is None:
-                result.add((x, y-1))
+            if board[i-1][j] is None:
+                result.add((i-1, j))
                 # 2 moves ahead
-                if y - 2 > -1:
-                    if not self.moved and board[x][y-2] is None:
-                        result.add((x, y-2))
+                if i - 2 > -1:
+                    if not self.moved and board[i-2][j] is None:
+                        result.add((i-2, j))
             # Can eat opponent piece
-            if x + 1 < 8:
-                if board[x+1][y-1] in self.opponents:
-                    result.add((x+1, y-1))
-            if x - 1 > -1:
-                if board[x-1][y-1] in self.opponents:
-                    result.add((x-1, y-1))
+            if j + 1 < 8:
+                if board[i-1][j+1] in self.opponents:
+                    result.add((i-1, j+1))
+            if j - 1 > -1:
+                if board[i-1][j-1] in self.opponents:
+                    result.add((i-1, j-1))
             # en passant
             ...
             return result
 
     def can_be_promoted(self):
         if self.side == "W":
-            return self.position[1] == 7
+            return self.position[0] == 7
         else:
-            return self.position[1] == 0
+            return self.position[0] == 0
 
 
 class Rook():
@@ -67,55 +67,55 @@ class Rook():
         self.opponents = opponents
 
     def __str__(self):
-        return f"RW at {self.position}"
+        return f"R at {self.position}"
 
     def available_moves(self, board):
         result = set()
-        x, y = self.position
-        # Check on the right
-        r = x + 1
+        i, j = self.position
+        # Check up
+        r = i + 1
         while r < 8:
-            if board[r][y] is None:
-                result.add((r, y))
-            elif board[r][y] in self.opponents:
-                result.add((r, y))
+            if board[r][j] is None:
+                result.add((r, j))
+            elif board[r][j] in self.opponents:
+                result.add((r, j))
                 break
             else:
                 break
             r += 1
-        # Check on the left
-        l = x - 1
-        while l > -1:
-            if board[l][y] is None:
-                result.add((l, y))
-            elif board[l][y] in self.opponents:
-                result.add((l, y))
-                break
-            else:
-                break
-            l -= 1
-        # Check up
-        u = y + 1
-        while u < 8:
-            if board[x][u] is None:
-                result.add((x, u))
-            elif board[x][u] in self.opponents:
-                result.add((x, u))
-                break
-            else:
-                break
-            u += 1
-        # Check down:
-        d = y - 1
+        # Check down
+        d = i - 1
         while d > -1:
-            if board[x][d] is None:
-                result.add((x, d))
-            elif board[x][d] in self.opponents:
-                result.add((x, d))
+            if board[d][j] is None:
+                result.add((d, j))
+            elif board[d][j] in self.opponents:
+                result.add((d, j))
                 break
             else:
                 break
             d -= 1
+        # Check right
+        r = j + 1
+        while r < 8:
+            if board[i][r] is None:
+                result.add((i, r))
+            elif board[i][r] in self.opponents:
+                result.add((i, r))
+                break
+            else:
+                break
+            r += 1
+        # Check left:
+        l = j - 1
+        while l > -1:
+            if board[i][d] is None:
+                result.add((i, d))
+            elif board[i][d] in self.opponents:
+                result.add((i, d))
+                break
+            else:
+                break
+            l -= 1
 # Find a way to exclude this when inherited by Queen
         # Check castling
         if self.can_castle:
@@ -125,16 +125,16 @@ class Rook():
     def can_castle(self, board, King_moved, KW_checked=False):
         # https://support.chess.com/article/266-how-do-i-castle
 # Missing rule "Your king can not pass through check"
-        y = self.position[1]
+        i = self.position[0]
         if not self.moved and not King_moved:
             if self.half == "L":
-                if any(board[x][y] for x in {1, 2, 3}):
+                if any(board[i][j] for j in {1, 2, 3}):
                     return None
-                return (3, y)
+                return (i, 3)
             else:
-                if any(board[x][y] for x in {5, 6}):
+                if any(board[i][j] for j in {5, 6}):
                     return None
-                return (5, y)
+                return (i, 5)
         else:
             return None
         
@@ -150,76 +150,78 @@ class Bishop():
 
     def available_moves(self, board):
         result = set()
-        x, y = self.position
+        i, j = self.position
         # Check up right
-        u = y + 1
-        r = x + 1
-        while u < 8 or r < 8:
-            if board[r][u] is None:
-                result.add((r, u))
-            elif board[r][u] in self.opponents:
-                result.add((r, u))
+        d = i + 1
+        r = j + 1
+        while d < 8 or r < 8:
+            if board[d][r] is None:
+                result.add((d, r))
+            elif board[d][r] in self.opponents:
+                result.add((d, r))
                 break
             else:
                 break
-            u += 1
+            d += 1
             r += 1
         # Check down right
-        d = y - 1
-        r = x + 1
+        d = i - 1
+        r = j + 1
         while d > -1 or r < 8:
-            if board[r][d] is None:
-                result.add((r, d))
-            elif board[r][d] in self.opponents:
-                result.add((r, d))
+            if board[d][r] is None:
+                result.add((d, r))
+            elif board[d][r] in self.opponents:
+                result.add((d, r))
                 break
             else:
                 break
-            d += 1
+            d -= 1
             r += 1
         # Check down left
-        d = y - 1
-        l = x - 1
+        d = i - 1
+        l = j + 1
         while d > -1 or l > -1:
-            if board[l][d] is None:
-                result.add((l, d))
-            elif board[r][d] in self.opponents:
-                result.add((l, d))
+            if board[d][l] is None:
+                result.add((d, l))
+            elif board[d][l] in self.opponents:
+                result.add((d, l))
                 break
             else:
                 break
-            d += 1
-            l += 1
+            d -= 1
+            l -= 1
         # Check up left
-        u = y + 1
-        l = x - 1
+        u = i + 1
+        l = j - 1
         while u < 8 or l > -1:
-            if board[l][u] is None:
-                result.add((r, d))
-            elif board[l][u] in self.opponents:
-                result.add((l, u))
+            if board[u][l] is None:
+                result.add((u, l))
+            elif board[u][l] in self.opponents:
+                result.add((u, l))
                 break
             else:
                 break
             u += 1
-            l += 1
+            l -= 1
+        
 
 
 class Knight():
-    def __init__(self, position):
+    def __init__(self, position, opponents):
         self.alive = True
         self.position = position
+        self.opponents = opponents
 
     def __str__(self):
         return f"NW at {self.position}"
 
-    def available_moves(self, board, white_pieces):
-        x, y = self.position
-        result = set((x+1, y+2), (x+2, y+1), (x+2, y-1), (x+1, y-2),
-                  (x-1, y-2), (x-2, y-1), (x-2, y+1), (x-1, y+2))
+    def available_moves(self, board):
+        i, j = self.position
+        result = set((i+1, j+2), (i+2, j+1), (i+2, j-1), (i+1, j-2),
+                  (i-1, j-2), (i-2, j-1), (i-2, j+1), (i-1, j+2))
         for move in result:
             try:
-                if board[move[0]][move[1]] in white_pieces:
+                if board[move[0]][move[1]] in self.opponents:
                     result.remove(move)
             except:
                 pass
@@ -253,10 +255,10 @@ class King():
     
     def available_moves(self, board):
         result = set()
-        x, y = self.position
+        i, j = self.position
         # Normal case
-        for i in range(x-1, x+2):
-            for j in range(y-1, y+2):
+        for i in range(i-1, i+2):
+            for j in range(j-1, j+2):
                 try:
                     if board[i][j] is None:
                         result.add((i, j))
@@ -274,11 +276,11 @@ class King():
         # https://support.chess.com/article/266-how-do-i-castle
 # Missing rule "Your king can not pass through check"
         result = set()
-        y = self.position[1]
+        i = self.position[0]
         if not self.moved and not KnightL_moved:
-            if not any(board[x][y] for x in {1, 2, 3}):
-                result.add((2, y))
+            if not any(board[i][j] for j in {1, 2, 3}):
+                result.add((i, 2))
         if not self.moved and not KnightR_moved:
-            if not any(board[x][y] for x in {5, 6}):
-                result.add((6, y))
+            if not any(board[i][j] for j in {5, 6}):
+                result.add((i, 6))
         return result
