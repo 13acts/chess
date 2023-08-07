d8106eecc6ddde3bba384f067ab5bcf42eff494c
13acts <tv2k@gmail.com>
2023-08-07 17:39:57 +0700

    Added chess,py

---
+++ chess.py
@@ -0,0 +1,21 @@
+from pieces import *
+
+
+# white pieces
+PW = Pawn() # represents white pawn 
+KW = " ♔ " # represents white king
+QW = " ♕ " # represents white queen 
+RW = " ♖ " # represents white rook 
+BW = " ♗ " # represents white bishop 
+NW = " ♘ " # represents white knight
+
+# black pieces
+PB = " ♟︎ " # represents black pawn
+KB = " ♚ " # represents black king
+QB = " ♛ " # represents black queen 
+RB = " ♜ " # represents black rook 
+BB = " ♝ " # represents black bishop 
+NB = " ♞ " # represents black knight
+BLACK = "BLACK" 
+WHITE = "WHITE"
+E = None # represents empty 
